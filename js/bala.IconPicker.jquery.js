5791ae60520ad2608038fe0f8fc74688eaaedeb8
balachuang <balachuang@gmail.com>
2014-08-09 06:03:03 +0800

    Code Initialize

---
+++ js/bala.IconPicker.jquery.js
@@ -0,0 +1,139 @@
+(function($) {
+
+	$.fn.IconPicker = function(parameter)
+	{
+		var __iconMargin__ = 3;
+		var __containerMargin__ = 3;
+		var __containerPadding__ = 5;
+
+		var __iconSize__ = 50;
+		var __currSelection__ = 0;
+		var __hasNullIcon__ = true;
+		var __containerPosition__ = 'right-4';
+		var __containerWidth__ = 5;
+		var __containerHeight__ = 3;
+		var __onDisplayIconChange__ = function(){};
+
+		var __displayerDiv__ = null;
+		var __containerDivDisp__ = null;
+		var __containerDiv__ = null;
+
+		if (parameter.iconSize != undefined) __iconSize__ = parameter.iconSize;
+		if (parameter.currSelection != undefined) __currSelection__ = parameter.currSelection;
+		if (parameter.hasNullIcon != undefined) __hasNullIcon__ = parameter.hasNullIcon;
+		if (parameter.containerPosition != undefined) __containerPosition__ = parameter.containerPosition;
+		if (parameter.containerWidth != undefined) __containerWidth__ = parameter.containerWidth;
+		if (parameter.containerHeight != undefined) __containerHeight__ = parameter.containerHeight;
+		if (parameter.onDisplayIconChange != undefined) __onDisplayIconChange__ = parameter.onDisplayIconChange;
+
+		this.each(function(){
+			var baseDiv = $(this);
+			if (baseDiv.prop('tagName') == 'DIV') {
+
+				// record all image urls and remove all image
+				var imgObjs = baseDiv.find('img');
+				var imgUrls = new Array();
+				imgObjs.each(function(){
+					imgUrls.push($(this).attr('src'));
+				});
+				imgObjs.remove();
+
+				// create icon-displayer and icon-container
+				var contHtml =
+					'<div class="balaIconPicker-icon-displayer" />         ' +
+					'<div class="balaIconPicker-icon-container-displayer"> ' +
+					'	<div class="balaIconPicker-icon-container" /></div>' ;
+				baseDiv.append(contHtml);
+				__displayerDiv__ = baseDiv.find('div.balaIconPicker-icon-displayer');
+				__containerDivDisp__ = baseDiv.find('div.balaIconPicker-icon-container-displayer');
+				__containerDiv__ = __containerDivDisp__.find('div.balaIconPicker-icon-container');
+
+				// add all image into icon-container.
+				var iconHtml = '';
+				if (__hasNullIcon__) iconHtml += '<div class="balaIconPicker-null-icon" />';
+				for (var n=0; n<imgUrls.length; ++n) iconHtml += '<div class="balaIconPicker-common-icon" style="background-image:url(\'' + imgUrls[n] + '\');" />';
+				__containerDiv__.append(iconHtml);
+
+				// apply dynamic CSS
+				baseDiv.find('div.balaIconPicker-null-icon').css({
+					'width':  __iconSize__,
+					'height': __iconSize__,
+					'background-size' : '' + __iconSize__ + 'px ' + __iconSize__ + 'px'
+				});
+				baseDiv.find('div.balaIconPicker-common-icon').css({
+					'width':  __iconSize__,
+					'height': __iconSize__,
+					'background-size' : '' + __iconSize__ + 'px ' + __iconSize__ + 'px'
+				});
+				__displayerDiv__.css({
+					'width':  (__iconSize__ + (__iconMargin__ * 2)),
+					'height': (__iconSize__ + (__iconMargin__ * 2)),
+					'background-size' : '' + __iconSize__ + 'px ' + __iconSize__ + 'px'
+				});
+				__containerDivDisp__.css({
+					'width':  (__iconSize__ + (__iconMargin__ * 2)) * __containerWidth__ + __containerMargin__,
+					'height': (__iconSize__ + (__iconMargin__ * 2)) * __containerHeight__ + __containerMargin__
+				});
+				__containerDiv__.css({
+					'width':  (__iconSize__ + (__iconMargin__ * 2)) * __containerWidth__ + __containerMargin__ + 17,
+					'height': (__iconSize__ + (__iconMargin__ * 2)) * __containerHeight__ + __containerMargin__ + 17
+				});
+
+				// change conatiner position
+				var displayerTop = __displayerDiv__.position().top;
+				var displayerLft = __displayerDiv__.position().left;
+				switch(__containerPosition__)
+				{
+				case 'right-4':
+					__containerDivDisp__.css({
+						'top':  displayerTop,
+						'left': displayerLft + __iconSize__ + (__iconMargin__ * 4)
+					});
+				}
+				__containerDivDisp__.hide();
+
+				// set displayer icon
+				__displayerDiv__.css({
+					'background-image': __containerDiv__.find('div:eq(' + __currSelection__ + ')').css('background-image')
+				});
+
+				// apply event handler
+				__displayerDiv__.click(function(){
+					if (__containerDivDisp__.is(':hidden')) {
+						$('div.balaIconPicker-icon-container-displayer').slideUp(100);
+						__containerDivDisp__.slideDown(200);
+					}else{
+						__containerDivDisp__.slideUp(100);
+					}
+				});
+				__containerDiv__.find('div').click(function(){
+					__displayerDiv__.css('background-image', $(this).css('background-image'));
+					__currSelection__ = $(this).index();
+					__containerDivDisp__.slideUp(100);
+
+					// trigger onDisplayChange event
+					__onDisplayIconChange__( __currSelection__, getSelectedUrl());
+				});
+			}
+		});
+
+		function getSelectedIndex(){ return __currSelection__; };
+		function getSelectedUrl(){
+			var retUrl = __containerDiv__.find('div:eq(' + __currSelection__ + ')').css('background-image');
+			var regex = new RegExp(/url\("?(.+)"?\)/);
+			retUrl = retUrl.split(regex)[1];
+			if (retUrl.indexOf('bala.IconPicker.nullIcon.png', retUrl.length-28) >= 0) retUrl = '';
+			return retUrl;
+		};
+		function setSelectedIndex(idx) {
+			if (idx <= __containerDiv__.find('div').length) {
+				__currSelection__ = idx;
+				__displayerDiv__.css({'background-image': getSelectedUrl()});
+			}
+		};
+		function setSelectedUrl(url){
+			__displayerDiv__.css({'background-image': url});
+		};
+	};
+
+})(jQuery);
